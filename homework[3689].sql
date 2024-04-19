20d82679351af9aa468ec9192cb8f979d9db9031
Kellen Rouff <KJR62@pitt.edu>
2024-04-18 23:16:39 -0500

    Add files via upload

---
+++ homework[3689].sql
@@ -0,0 +1,56 @@
+select * from publishers;
+select * from authors;
+select * from titles;
+select * from sales;
+select * from stores;
+
+--1 For each book published by New Moon Books, list the book title and price. 
+select p.pub_name, t.title, t.price from publishers p join titles t on p.pub_id=t.pub_id and p.pub_name='New Moon Books';
+
+--2 Turn Question 1 into a view named NewMoonBooks.
+drop procedure NewMoonBooks go create procedure NewMoonBooks @title varchar(100), @price int, @pub_name varchar(100)
+AS BEGIN SELECT p.pub_name, t.title, t.price from publishers p join titles t on p.pub_id=t.pub_id and p.pub_name=@pub_name;
+
+--3 Query this view for all books that cost more than $8.
+select p.pub_name, t.title, t.price from publishers p join titles t on p.pub_id=t.pub_id and t.price>=8;
+
+--4. List the book title and price of each book published by New Moon Books that has a book price of at least $10. 
+select p.pub_name, t.title, t.price from publishers p join titles t on p.pub_id=t.pub_id and p.pub_name='New Moon Books' and t.price>=10;
+
+--5. List the book title for each book that has the type psychology and that is published by Binnet & Hardley.
+select p.pub_name, t.title, t.type from publishers p join titles t on p.pub_id=t.pub_id and p.pub_name='Binnet & Hardley' and t.type='psychology';
+
+--6. Find the book title for each book written by Ann Dull. Use the IN operator in your query. 
+select titles.title from titles join titleauthor on titles.title_id=titleauthor.title_id join authors on titleauthor.au_id=authors.au_id where authors.au_fname in ('Ann') and authors.au_lname in ('Dull');
+
+--7. Repeat Exercise 6, but this time use the EXISTS operator in your query. 
+SELECT t.title FROM titles t join titleauthor ta on t.title_id=ta.title_id WHERE EXISTS (SELECT a.au_id FROM authors a WHERE ta.au_id=a.au_id and a.au_fname in ('Ann') and a.au_lname in ('Dull')); 
+
+--8. Find the book title for each book sold at Doc-U-Mat and written by Charlene Locksley. 
+select t.title, au.au_fname, au_lname, s.stor_name from stores s, titles t join titleauthor ta on t.title_id=ta.title_id join authors au on ta.au_id=au.au_id where au.au_fname='Charlene' and au.au_lname='Locksley' and s.stor_name='Doc-U-Mat: Quality Laundry and Books'
+
+--9. List the title for each pair of books that have the same price. ( For example, one such pair would be “Fifty Years in Buckingham Palace Kitchens” and “Cooking with Computers”, because the price of both books is $11.95.) The first book code listed should be the major sort key, and the second book code should be the minor sort key.   Exclude duplicates.
+
+--10. Find the book title and author last name for all books published in the USA. 
+select titles.title, publishers.country from titles left outer join publishers on titles.pub_id=publishers.pub_id where publishers.country='USA';
+
+--11. Repeat Exercise 10, but this time list only business books. 
+select titles.title, titles.type, publishers.country from titles left outer join publishers on titles.pub_id=publishers.pub_id where publishers.country='USA' and titles.type='business';
+
+--12. Find the book title for each book whose price is more than $10 or that was published in Boston. 
+select titles.title, titles.price, publishers.city from titles, publishers where price>='10' or city='Boston';
+
+--13. Find the book title for each book whose price is more than $10 and that was published in Boston. 
+select titles.title, titles.price, publishers.city from titles, publishers where price>='10' and city='Boston';
+
+--14. Find the book title for each book whose price is more than $10 but that was not published in Boston.
+select titles.title, titles.price, publishers.city from titles, publishers where price>='10' and NOT city='Boston';
+
+--15. Find the book title for each book whose price is greater than the book price of every book that has the type business. 
+SELECT * FROM titles WHERE price=(select max(price) from titles t where t.type='business')
+
+--16. Find the book title for each book whose price is greater than the price of at least one book that has the type business. 
+select MAX(titles.price), titles.title from titles where type='business' group by titles.type, titles.title;
+
+--17. List the book title and qty (sales table) for each book sold at Barnum’s . Be sure each book is included, regardless of whether there are any copies of the book currently sold by Barnum’s. Order the output by title_id.
+select t.title, coalesce(s.qty,0) from titles t left outer join sales s on t.title_id=s.title_id left outer join stores st on st.stor_id=s.stor_id and st.stor_name='Barnum''s'
---
+++ videogamecollectiondatabase[3688].sql
@@ -0,0 +1,182 @@
+﻿drop database VideoGameCollection
+go
+Create database VideoGameCollection
+go
+use VideoGameCollection
+
+create table VideoGame (
+Unique_ID int identity (100,1) primary key,
+Name nvarchar(50) not null UNIQUE,
+Release_Date date,
+ESRB_Rating nvarchar(3)
+);
+
+create table Console (
+Serial_Numb nvarchar(50) primary key,
+Name nvarchar(50) not null,
+Model nvarchar(50) not null,
+Release_Date date,
+);
+
+create table Borrower (
+Borrower_ID int identity (100,1) primary key,
+FName nvarchar(50) not null,
+MI nvarchar(1),
+LName nvarchar(50) not null,
+Address nvarchar(50) not null,
+city nvarchar(20) not null,
+state nvarchar(2),
+country nvarchar(20) not null,
+zip_code nvarchar(5) not null,
+phone_Numb nvarchar(15) not null,
+date_borrowed date not null,
+date_returned date
+);
+
+create table Genre (
+Genre_ID int identity (100,1) primary key,
+Name nvarchar(50) not null UNIQUE,
+); 
+
+create table Developer (
+Name nvarchar(200) primary key,
+est_date date,
+business_status nvarchar(50) not null,
+Address nvarchar(50) not null,
+city nvarchar(20) not null,
+state nvarchar(2),
+country nvarchar(20) not null,
+zip_code nvarchar(20) not null
+);
+
+create table GameDeveloper (
+Name nvarchar(200) FOREIGN KEY REFERENCES Developer(Name),
+Unique_ID int FOREIGN KEY REFERENCES VideoGame(Unique_ID)
+);
+
+create table GameGenre (
+Genre_ID int FOREIGN KEY REFERENCES Genre(Genre_ID),
+Unique_ID int FOREIGN KEY REFERENCES VideoGame(Unique_ID)
+);
+
+create table ConsoleGame (
+Unique_ID int FOREIGN KEY REFERENCES VideoGame(Unique_ID),
+Serial_Numb nvarchar(50)  FOREIGN KEY REFERENCES Console(Serial_Numb)
+);
+
+create table GameBorrow(
+Unique_ID int FOREIGN KEY REFERENCES VideoGame(Unique_ID),
+Borrower_ID int FOREIGN KEY REFERENCES Borrower(Borrower_ID)
+);
+
+Create table ConsoleBorrow(
+Serial_Numb nvarchar(50) FOREIGN KEY REFERENCES Console(Serial_Numb),
+Borrower_ID int FOREIGN KEY REFERENCES Borrower(Borrower_ID)
+);
+
+create index Contact on borrower(Fname,Mi,Lname,address,city,state,country,zip_code,phone_numb);
+create index daddress on developer(address,city,state,country,zip_code);
+create index BInfo on developer(business_status, est_date);
+create index Gname on genre(Name);
+CREATE UNIQUE INDEX name on developer(name);
+CREATE UNIQUE INDEX snum on console(serial_numb);
+CREATE UNIQUE INDEX BID on borrower(borrower_ID);
+CREATE UNIQUE INDEX VID on videoGame(Unique_ID);
+CREATE UNIQUE INDEX GID on Genre(Genre_ID);
+
+go
+create view ConsoleNameMod 
+as
+select Name, Model
+from Console
+go
+
+go
+CREATE PROCEDURE AllDevelopers 
+@country nvarchar(20) 
+AS BEGIN SELECT * FROM Developer WHERE country = @country 
+END; 
+go
+
+Insert into Borrower (FName, MI, LName, Address, city, state, country, zip_code, phone_Numb,date_borrowed) VALUES ('Steph','E','Morra','11 Boss Ave','Bradford','PA','USA','16701','2035201276','4/7/2019');
+Insert into Borrower (FName, MI, LName, Address, city, state, country, zip_code, phone_Numb,date_borrowed) VALUES ('Kellen','J','Rouff','11 Boss Ave','Bradford','PA','USA','16701','4754953305','4/14/2019');
+insert into Console (Name, Model, Release_Date, Serial_Numb) VALUES ('PS4','Standard','11/15/2013','CUH-1012A');
+insert into Developer (Name, est_date, business_status, Address, city,state, country,zip_code) VALUES ('Rockstar North','2002','Active','4 622, Broadway','New York','NY','USA','10012');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('GTA 5','M','09/17/2013');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Witcher 3','M','3/19/2015');
+insert into Developer (Name, est_date, business_status, Address, city,country,zip_code) VALUES ('CD Project Red','2002','Active','Jagiellonska 74','Warsaw','Poland','05-077')
+insert into Console (Name, Model, Release_Date, Serial_Numb) VALUES ('Nintendo Switch','First Gen','5/13/2017','XAW70000000000');
+insert into Console (Name, Model, Release_Date, Serial_Numb) VALUES ('Nintendo Switch','First Gen','5/13/2017','XAW78159320121');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Nier Automata','M','02/23/2017');
+insert into Developer (Name, est_date, business_status, Address, city,country,zip_code) VALUES ('PlatniumGames','2007','Acticve','8F Tower West, Umeda Sky Building','Osaka','Japan','531-6108');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Legend of Zelda Breath of the Wild','T','3/3/2017')
+insert into Developer (Name, est_date, business_status, Address, city,country,zip_code) VALUES ('Nintendo Entertainment Planning & Development','2015','Active','２−１ Higashikujō Minamimatsudachō','Kyoto','Japan','601-8502');
+Insert into Genre (name) Values ('Action');
+Insert into Genre (name) Values ('Adventure');
+Insert into Genre (name) Values ('MMO');
+Insert into Genre (name) Values ('MOBA');
+Insert into Genre (name) Values ('RPG');
+Insert into Genre (name) Values ('FPS');
+Insert into Genre (name) Values ('Simulation');
+Insert into Genre (name) Values ('Horror');
+Insert into Genre (name) Values ('Puzzle');
+Insert into Genre (name) Values ('Sandbox');
+Insert into Genre (name) Values ('Metriodvania');
+Insert into Genre (name) Values ('Tactical');
+Insert into Genre (name) Values ('Open World');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Devil May Cry 5','M','3/8/2019');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Sekiro Shadows Die Twice','M','3/22/2019');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Legend of Zelda Majoras Mask','T','4/27/2000');
+insert into Developer (Name, est_date, business_status, Address, city,country,zip_code) values ('Capcom','2001','Active','3-chōme-1-番３号 Uchihiranomachi','Osaka','Japan','540-0037');
+insert into GameGenre (Genre_ID, Unique_ID) VALUES ('112','100');
+insert into GameGenre (Genre_ID, Unique_ID) VALUES ('109','100');
+insert into GameDeveloper (Unique_ID, Name) VALUES ('100','Rockstar North');
+insert into ConsoleGame (Unique_ID, Serial_Numb) VALUES ('100','CUH-1012A');
+insert into ConsoleBorrow(Serial_Numb,Borrower_ID) VALUES ('CUH-1012A','100');
+insert into GameBorrow(Unique_ID,Borrower_ID) VALUES ('100','100');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Dark Souls','M','9/22/2011');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Dark Souls 2','M','5/11/2014');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Dark Souls 3','M','5/24/2016');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Bloodborne','M','5/24/2015');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('World of Warcraft','T','8/14/2018');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Warcraft 3 Reign of Chaos','T','7/3/2002');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Legend Of Zelda Ocarina of Time','T','11/21/1998');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Crash Bandicoot N.sane Trilogy','T','6/30/2017');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Final Fantasy X/X-2 Remastered','T','12/05/2013');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Warcraft 3 Frozen Throne','T','7/1/2001');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Sonic the Hedgehog','T','6/23/1991');
+insert into VideoGame (Name, ESRB_Rating, Release_Date) VALUES ('Toejam and Earl','T','3/12/1991');
+
+
+select * from Console;
+select * from Developer;
+select * from VideoGame;
+Select * from Genre;
+select * from Borrower;
+select * from GameGenre;
+select * from GameDeveloper;
+select * from ConsoleGame;
+select * from ConsoleBorrow;
+select * from GameBorrow;
+
+go
+CREATE PROCEDURE UpdateBorrower
+@borrowerID int,
+@FName nvarchar(50) = null,
+@MI nvarchar(1) = null,
+@LName nvarchar(50) = null,
+@Address nvarchar(50) = null,
+@city nvarchar(20) = null,
+@state nvarchar(2) = null,
+@country nvarchar(20) = null,
+@zip_code nvarchar(5) = null,
+@phone_Numb nvarchar(15) = null,
+@date_borrowed date = null,
+@date_returned date = null
+AS BEGIN 
+Update Borrower 
+set FName= isnull (@FName, FName), MI=isnull (@MI, MI), LName=isnull (@LName, LName), Address=isnull (@Address, Address), city=isnull (@city, city), state=isnull (@state, state), country=isnull (@country, country), zip_code=isnull (@zip_code, zip_code), phone_Numb=isnull (@phone_Numb, phone_Numb), date_borrowed=isnull (@date_borrowed, date_borrowed), date_returned=isnull (@date_returned, date_returned)
+where Borrower_ID=@borrowerID
+END
+go
+
